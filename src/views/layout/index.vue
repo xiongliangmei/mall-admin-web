c7344f8812d1133b2099465809bccd8752d61e21
Liang.Xiong <137010393@qq.com>
2020-09-14 17:42:08 +0800

    第一次提交

---
+++ src/views/layout/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="app-wrapper" :class="classObj">
+        <div class="siderbar-container">
+
+        </div>
+        <div class="main-container">
+
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'layout'
+}
+</script>
+<style rel="stylesheet/scss" lang="scss" scoped>
+  @import "src/styles/mixin.scss";
+  .app-wrapper {
+    @include clearfix;
+    position: relative;
+    height: 100%;
+    width: 100%;
+  }
+</style>
---
+++ src/views/login/login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="login-container">
+    <svg-icon icon-class="login-mall" style="width: 56px;height: 56px;color: #409EFF"></svg-icon>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+
+</style>
